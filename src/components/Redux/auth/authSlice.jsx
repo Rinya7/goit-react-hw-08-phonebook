fe52fdbbee58a49e7328f1ad64ac16bae08185f9
Rinya7 <v.dmitriev0712@gmail.com>
2023-08-24 15:04:27 +0200

    pending+rejected

--- src/components/Redux/auth/authSlice.jsx
+++ src/components/Redux/auth/authSlice.jsx
@@ -7,29 +7,62 @@ const authSlice = createSlice({
     user: { name: null, email: null },
     token: null,
     isLogged: false,
+    isFetchingCurrentUser: false,
     isRefreshing: false,
+    isLoadingUser: false,
+    errorUser: null,
   },
   extraReducers: {
+    [signUp.pending](state) {
+      state.isLoadingUser = true;
+    },
     [signUp.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLogged = true;
     },
-
+    [signUp.rejected](state, action) {
+      state.errorUser = action.payload;
+      state.isLoadingUser = false;
+    },
+    //=======================================================================
+    [logIn.pending](state) {
+      state.isLoadingUser = true;
+    },
     [logIn.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLogged = true;
     },
+    [logIn.rejected](state, action) {
+      state.errorUser = action.payload;
+      state.isLoadingUser = false;
+    },
 
+    //==============================================================
+    [logOut.pending](state) {
+      state.isLoadingUser = true;
+    },
     [logOut.fulfilled](state) {
       state.user = { name: null, email: null };
       state.token = null;
       state.isLogged = false;
     },
+    [logOut.rejected](state, action) {
+      state.errorUser = action.payload;
+      state.isLoadingUser = false;
+    },
+    //==============================================================
+    [refreshUser.pending](state) {
+      state.isLoadingUser = true;
+    },
     [refreshUser.fulfilled](state, action) {
       state.user = action.payload;
       state.isLogged = true;
+    },
+    [refreshUser.rejected](state, action) {
+      state.errorUser = action.payload;
+      state.isLoadingUser = false;
     },
   },
 });
